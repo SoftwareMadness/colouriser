0e3f14e2b5fbdfce126209460d2c09c487ccfd46
SoftwareMadness <44232840+AnthonyOpenSource@users.noreply.github.com>
2022-06-02 21:24:57 +0200

    Update train.py

--- train.py
+++ train.py
@@ -1,6 +1,10 @@
 import libcygtrn
 from libcygtrn import CYCLES
+import yaml
+
+with open("config.yml","r") as f:
+    settings = yaml.safe_load(f)
 
 cyc = CYCLES()
 
-cyc.train()
+cyc.train(cuda = settings["UseCUDA"],dataroot = settings["root"],dataset_p = settings["set"],image_size=settings["i_size"],batch_size=settings["bs"],epochs=settings["ep"],decay_epochs = settings["ep"] / 2,print_freq=settings["pf"])
