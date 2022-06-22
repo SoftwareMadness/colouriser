4962ec12b0d9eff7556238e91e9bd9678a1f04d5
SoftwareMadness <44232840+AnthonyOpenSource@users.noreply.github.com>
2022-06-22 17:19:43 +0200

    Update cam.py

--- cam.py
+++ cam.py
@@ -13,6 +13,18 @@ cyc = CYCLES()
 
 netG_A2B,netG_B2A,netD_A,netD_B = cyc.retrieve_nets()
 
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print('Using device:', device)
+
+netG_A2B,netG_B2A,netD_A,netD_B = cyc.retrieve_nets()
+
+netG_A2B = netG_A2B.to(device)
+netG_B2A = netG_B2A.to(device)
+netD_A = netD_A.to(device)
+netD_B = netD_B.to(device)
+
+
 pre_process = transforms.Compose([transforms.Resize(image_size),
                                   transforms.ToTensor(),
                                   transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
@@ -41,7 +53,7 @@ while True:
         break
     if key == ord('c'):
         nimg = pre_process(Image.fromarray(gray)).unsqueeze(0)
-        calc = netG_B2A(nimg).detach()
+        calc = netG_B2A(nimg.to(device)).detach().to("cpu")
         vutils.save_image(calc, "result.png", normalize=True)
         calc = cv2.imread("result.png")
         cv2.imshow('Processed, press any key to exit',calc)
