4e79fd6fd673090c51dfc1fde577cae87af815cd
SoftwareMadness <44232840+AnthonyOpenSource@users.noreply.github.com>
2022-06-22 17:19:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,15 @@ image_size = 256
 
 cyc = CYCLES()
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print('Using device:', device)
+
 netG_A2B,netG_B2A,netD_A,netD_B = cyc.retrieve_nets()
+
+netG_A2B = netG_A2B.to(device)
+netG_B2A = netG_B2A.to(device)
+netD_A = netD_A.to(device)
+netD_B = netD_B.to(device)
 
 pre_process = transforms.Compose([transforms.Resize(image_size),
                                   transforms.ToTensor(),
@@ -48,7 +56,7 @@ if func[0] == '2':
 	net_f = netG_A2B
 
 nimg = pre_process(Image.fromarray(cv2.imread(img))).unsqueeze(0)
-calc = net_f(nimg).detach()
+calc = net_f(nimg.to(device)).to("cpu").detach()
 vutils.save_image(calc, "result.png", normalize=True)
 calc = cv2.imread("result.png")
 cv2.imshow('frame', calc)
